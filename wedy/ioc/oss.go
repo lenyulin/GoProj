f6a37279d8066a457bbcfbc0377b89f5e856abd2
雷育林 <lyl694550795@msn.cn>
2025-07-16 18:17:55 +0800

    Changes

--- wedy/ioc/oss.go
+++ wedy/ioc/oss.go
@@ -7,16 +7,16 @@ import (
 )
 
 func InitOSS() *cos.Client {
-	u, _ := url.Parse("https://testmedias-1253517205.cos.ap-guangzhou.myqcloud.com")
+	u, _ := url.Parse("https://testmedias-xxxxxxxxxx.cos.ap-guangzhou.myqcloud.com")
 	su, _ := url.Parse("https://service.cos.myqcloud.com")
 	b := &cos.BaseURL{BucketURL: u, ServiceURL: su}
 	client := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
 			// 通过环境变量获取密钥
 			// 环境变量 SECRETID 表示用户的 SecretId，登录访问管理控制台查看密钥，https://console.cloud.tencent.com/cam/capi
-			SecretID: "AKIDXvATgibKqIwTr38DD4naHo9OTL0ShjZC", // 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+			SecretID: "xxxxxxxxxx", // 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
 			// 环境变量 SECRETKEY 表示用户的 SecretKey，登录访问管理控制台查看密钥，https://console.cloud.tencent.com/cam/capi
-			SecretKey: "WsE11tmhT2Rkz1qX3MVXUL4d2VuWzJRQ", // 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+			SecretKey: "xxxxxxxxxx", // 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
 		},
 	})
 	return client
